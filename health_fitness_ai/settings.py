e9cffab8779c8952bc88b05ef4c1823f11045dc8
nithinhero <nithin2003.p@gmail.com>
2025-04-24 16:55:35 +0530

    Update settings.py

--- health_fitness_ai/settings.py
+++ health_fitness_ai/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['healthapp-53zm.onrender.com']
 
 
 
