e5d1fdc01c514940d7e4da25651579b4e04c95e2
nithinhero <nithin2003.p@gmail.com>
2025-04-30 21:29:43 +0530

    Update admin.py

--- meals/admin.py
+++ meals/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-from .models import  MealLog
-admin.site.register(MealLog)
+from .models import Meal
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    list_display = ('id', 'input_type', 'food_name', 'quantity', 'units', 'time', 'created_at')
+    list_filter = ('input_type', 'time', 'units')
+    search_fields = ('food_name',)
